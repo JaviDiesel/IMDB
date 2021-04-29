5158bf208448add833acca0d9858671e76bf5561
JaviDiesel <fjramos13@hotmail.com>
2021-04-29 11:10:33 +0200

    class Imdb

--- mainImdb.ts
+++ mainImdb.ts
@@ -1,8 +1,10 @@
+import { Imdb } from './Imdb';
 import {Movie} from './Movie';
 
 let movie_1:Movie = new Movie('Joker', 2019, 'estadounidense', 'suspense');
 let movie_2:Movie = new Movie('Parasito', 2019, 'coreana', 'suspense');
 
 let peliculas:Movie[] = [movie_1, movie_2];
+let imdb:Imdb = new Imdb(peliculas);
 
-console.log(peliculas);
+console.log(imdb);
